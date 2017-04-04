50d3968b2dbbd5378eacbc77682580857700d3f2
MasterT1 <andre.brathwaite5@mycavehill.uwi.edu>
2017-04-04 10:42:48 -0400

    writetext completed

--- writetext.cpp
+++ writetext.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main( )
@@ -7,7 +8,14 @@ int main( )
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
- 
+   ofstream textf;
+   
+   textf.open("abc.txt");
+   textf<<a<<"\n";
+   textf<<b<<"\n";
+   textf<<c<<"\n";
+   textf.close();
+   	
    cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
 
    cout<<endl<<endl;
